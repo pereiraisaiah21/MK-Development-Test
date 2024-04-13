67abadd4336aec38de9749b7728dcea77ba673d9
pereisaiah21 <isaiahpereira@proton.me>
2024-04-13 13:51:46 -0300

    #4 - Adding provider component.

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./styles/main.scss"
 
+
+import { Provider } from 'react-redux'
+import store from './redux/store';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
 )
